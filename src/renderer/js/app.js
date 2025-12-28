fd1f71c86cfda148735acb8db6e2904a0547f8c2
patryk-szczech <123065037+patryk-szczech@users.noreply.github.com>
2025-12-28 16:18:06 +0100

    Create app.js

---
+++ src/renderer/js/app.js
@@ -0,0 +1,5 @@
+console.log('🎉 Aplikacja Kwitariusz Szkoły załadowana!');
+
+document.addEventListener('DOMContentLoaded', async () => {
+    console.log('DOM loaded. Window API available:', window.api !== undefined);
+});
